badf64622e47420073e0feba98359e279fafb58f
Serhii  Zadorozhnyi <zadorozhnyi.sergey2@gmail.com>
2022-11-16 19:55:05 +0900

    added request.http and routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return [];
+  }
+
+  @Post()
+  createMessage() {
+    return 'This action adds a new message';
+  }
+
+  @Get(':id')
+  getMessageById() {
+    return 'This action returns a message';
+  }
+}
