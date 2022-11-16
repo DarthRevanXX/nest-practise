0aef8e8d9c3a691c033859002e5d9463403234ac
Serhii  Zadorozhnyi <zadorozhnyi.sergey2@gmail.com>
2022-11-16 21:37:56 +0900

    refactor using dependency injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,26 +1,35 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-  constructor() {
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
 
   @Get()
-  listMessages() {
-    return this.messagesService.findAll();
+  async listMessages() {
+    return await this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {
-    return this.messagesService.create(body.content);
+  async createMessage(@Body() body: CreateMessageDto) {
+    return await this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessageById(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessageById(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
